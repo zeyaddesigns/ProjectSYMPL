544b401be66cd5708b81d823bed5cb6597953eb5
ztaa1 <z.t.alsultan@gmail.com>
2022-01-14 16:09:11 +0000

    PlayerController
    Add movement and roll functions to the player and finished setting up the camera to follow

--- SYMPL/Assets/Scripts/PlayerController.cs
+++ SYMPL/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float throwSpeed = 25f;
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -400f;
+
+    float xThrow, yThrow;
 
     void Update()
     {
-    
-        float xThrow = Input.GetAxis("Horizontal") * Time.deltaTime * throwSpeed;
-        float yThrow = Input.GetAxis("Vertical") * Time.deltaTime * throwSpeed;
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+
+    void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlFlow = yThrow * controlPitchFactor;
+
+        float pitch = pitchDueToPosition + pitchDueToControlFlow;
+        float yaw   = 0f;
+        float roll  = 0f;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    void ProcessTranslation()
+    {
+        xThrow = Input.GetAxis("Horizontal") * Time.deltaTime * throwSpeed;
+        yThrow = Input.GetAxis("Lift") * Time.deltaTime * throwSpeed;
+        float zThrow = Input.GetAxis("Vertical") * Time.deltaTime * throwSpeed;
         
-        float newXPos = transform.localPosition.x + xThrow;
-        float newYPos = transform.localPosition.y + yThrow;
+        float newXPosition = transform.localPosition.x + xThrow;
+        float newZPosition = transform.localPosition.z + zThrow;
+        float newYPosition = transform.localPosition.y + yThrow;
 
-        transform.localPosition = new Vector3 (newXPos, newYPos, transform.localPosition.z);
-
+        transform.localPosition = new Vector3(newXPosition, newYPosition, newZPosition);
     }
 }
